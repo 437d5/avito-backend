85b4141754627b430a896c0d4929a45b1d9a5113
437d5 <uladzislaukaplan@gmail.com>
2024-09-08 20:16:20 +0300

    added empty files

---
+++ go.mod
@@ -0,0 +1,3 @@
+module zadanie-6105
+
+go 1.23.0
---
+++ internal/config/config.go
@@ -0,0 +1,36 @@
+package config
+
+import (
+	"errors"
+	"os"
+)
+
+type Config struct{
+	SERVER_ADDRESS string
+	POSTGRES_CONN string
+	POSTGRES_JDBC_URL string
+	POSTGRES_USERNAME string
+	POSTGRES_PASSWORD string
+	POSTGRES_HOST string
+	POSTGRES_PORT string
+	POSTGRES_DATABASE string
+}
+
+func NewConfig() (*Config, error) {
+	config := Config{
+		SERVER_ADDRESS: os.Getenv("SERVER_ADDRESS"),
+		POSTGRES_CONN: os.Getenv("POSTGRES_CONN"),
+		POSTGRES_JDBC_URL: os.Getenv("POSTGRES_JDBC_URL"),
+		POSTGRES_USERNAME: os.Getenv("POSTGRES_USERNAME"),
+		POSTGRES_PASSWORD: os.Getenv("POSTGRES_PASSWORD"),
+		POSTGRES_HOST: os.Getenv("POSTGRES_HOST"),
+		POSTGRES_PORT: os.Getenv("POSTGRES_PORT"),
+		POSTGRES_DATABASE: os.Getenv("POSTGRES_DATABASE"),
+	}
+
+	if config.SERVER_ADDRESS == "" {
+		return nil, errors.New("SERVER_ADDRESS variable is not provided")
+	}
+
+	return &config, nil
+}
